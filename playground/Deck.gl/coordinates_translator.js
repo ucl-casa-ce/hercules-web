a7bf513f43c749e1d78d2929df6ae81a6fad36a6
Yaman Kalaji <syk.yaman@gmail.com>
2024-01-19 00:38:50 +0000

    Upgrade the coordinates translator to use JSON object as an input

--- playground/Deck.gl/coordinates_translator.js
+++ playground/Deck.gl/coordinates_translator.js
@@ -1,23 +1,56 @@
-function translateCoordinates(i, w) {
+function translatePathCoordinates(jsonObject) {
     // Define the ranges for i, w, longitude, and latitude
     const iRange = { min: 0, max: 35 };
     const wRange = { min: 0, max: 20 };
     const longitudeRange = { min: 40.65000, max: 40.75000 };
     const latitudeRange = { min: -74.0066, max: -73.97000 };
 
-    // Calculate the ratio of i and w within their respective ranges
-    const iRatio = (i - iRange.min) / (iRange.max - iRange.min);
-    const wRatio = (w - wRange.min) / (wRange.max - wRange.min);
+    // Function to translate coordinates
+    function translateCoordinates(i, w) {
+        const iRatio = (i - iRange.min) / (iRange.max - iRange.min);
+        const wRatio = (w - wRange.min) / (wRange.max - wRange.min);
+        const longitude = longitudeRange.min + iRatio * (longitudeRange.max - longitudeRange.min);
+        const latitude = latitudeRange.min + wRatio * (latitudeRange.max - latitudeRange.min);
+        return [longitude, latitude];
+    }
 
-    // Use the ratios to calculate the longitude and latitude
-    const longitude = longitudeRange.min + iRatio * (longitudeRange.max - longitudeRange.min);
-    const latitude = latitudeRange.min + wRatio * (latitudeRange.max - latitudeRange.min);
+    // Iterate over each object in the JSON array
+    for (let obj of jsonObject) {
+        // Translate each pair of coordinates in the 'path' array
+        for (let i = 0; i < obj.path.length; i++) {
+            obj.path[i] = translateCoordinates(obj.path[i][0], obj.path[i][1]);
+        }
+    }
 
-    // Return the calculated longitude and latitude
-    return [longitude, latitude];
+    // Return the modified JSON object
+    return jsonObject;
 }
 
-
 //Usage Example:
-//let [longitude, latitude] = translateCoordinates(10, 5);
-//console.log(`Longitude: ${longitude}, Latitude: ${latitude}`);
+//let jsonObject = [
+//    {
+//      "vendor": 0,
+//      "path": [
+//        [
+//          0,
+//          0
+//        ],
+//        [
+//          3.738,
+//          6.596
+//        ],
+//        [
+//          5.389,
+//          5.838
+//        ]
+//      ],
+//      "timestamps": [
+//             0,
+//             60,
+//             60,
+//      ]
+//    }
+//  ];
+//
+//  let translatedJsonObject = translatePathCoordinates(jsonObject);
+//  console.log(JSON.stringify(translatedJsonObject, null, 2));
