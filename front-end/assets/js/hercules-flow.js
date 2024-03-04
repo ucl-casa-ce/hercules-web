e9281135fd88c2330586dcb79db2c1aea7f720d2
Yaman Kalaji <syk.yaman@gmail.com>
2024-03-04 20:56:13 +0000

    Revert "Try to overlay paths with opacity"
    This reverts commit 08d728f7200ba8359dbc2131100ee430176aa71d.

--- front-end/assets/js/hercules-flow.js
+++ front-end/assets/js/hercules-flow.js
@@ -3096,6 +3096,10 @@ const COLOR_RANGE = [
         function loadMapData(data) {
             const TripsLayer = deck.TripsLayer;
             const LOOP_LENGTH = 5920;
+            const VENDOR_COLORS = [
+                [255, 0, 0],
+                [0, 0, 0], 
+            ];
             let currentTime = 0;
             
             const tripProps = {
@@ -3104,8 +3108,8 @@ const COLOR_RANGE = [
                 getPath: (d) => d.path,
                 getTimestamps: (d) => d.timestamps,
                 getColor: (d) => d.vendor,//d.vendor
-                opacity: 0.005,
-                widthMinPixels: 2,
+                opacity: 3,
+                widthMinPixels: 4,
                 trailLength: 250000,
                 currentTime,
                 shadowEnabled: false,
